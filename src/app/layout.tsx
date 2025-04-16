589f449a291173bf1595d114e148d7f64e32abb4
Nenad Stojanovic <nenad.stojanovic87@gmail.com>
2025-04-16 23:50:05 +0200

    Added toast globally

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import './globals.css';
 import { METADATA } from '../data/metadata';
 import { poppins } from '@/lib/fonts';
 import ClientProviders from '@/components/clientProviders/CllientProviders';
+import { Toaster } from 'react-hot-toast';
 
 export const metadata: Metadata = {
   title: METADATA.title + METADATA.bTitle,
@@ -17,7 +18,21 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
   return (
     <html lang="en">
       <body className={poppins.className}>
-        <ClientProviders>{children}</ClientProviders>
+        <ClientProviders>
+          {children}{' '}
+          <Toaster
+            position="top-right"
+            toastOptions={{
+              duration: 4000,
+              style: {
+                background: '#fff',
+                color: '#000',
+                border: '1px solid #e2e8f0',
+                fontSize: '14px',
+              },
+            }}
+          />
+        </ClientProviders>
       </body>
     </html>
   );
