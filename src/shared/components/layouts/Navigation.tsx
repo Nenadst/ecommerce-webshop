7e922a18af92bf2b4c1f2e5b554b0061b2abc888
Nenad Stojanovic <nenad.stojanovic87@gmail.com>
2025-09-05 23:04:08 +0200

    Added dropdown for categories on the homepage

--- src/shared/components/layouts/Navigation.tsx
+++ src/shared/components/layouts/Navigation.tsx
@@ -1,19 +1,14 @@
 import Link from 'next/link';
 import React from 'react';
 import { ChevronDownIcon } from '../icons';
+import { CategoriesDropdown } from '../navigation/CategoriesDropdown';
 
 const Navigation = () => {
   return (
     <div className="w-full h-16 bg-zinc-100 sticky top-16 z-50 hidden lg:block">
       <div className="flex justify-between">
         <div className="flex flex-wrap">
-          <Link
-            href="/products"
-            className="ml-16 w-52 h-16 bg-amber-500 flex items-center justify-center hover:bg-amber-600"
-          >
-            <div className="text-white font-semibold text-base mr-3">Browse categories</div>
-            <ChevronDownIcon />
-          </Link>
+          <CategoriesDropdown className="ml-16" />
           <div className="mr-20" />
           <Link
             href="/"
