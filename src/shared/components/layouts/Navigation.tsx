5817be6f21eceabbe94bf91bfd5771cef5c208a4
Nenad Stojanovic <nenad.stojanovic87@gmail.com>
2025-10-03 12:09:58 +0200

    Removed unused links and changed text in navigation

--- src/shared/components/layouts/Navigation.tsx
+++ src/shared/components/layouts/Navigation.tsx
@@ -17,7 +17,6 @@ const Navigation = () => {
             <div className="text-slate-500 font-semibold text-base group-hover:text-white">
               Home
             </div>
-            <ChevronDownIcon />
           </Link>
           <Link
             href="/products"
@@ -25,15 +24,6 @@ const Navigation = () => {
           >
             <div className="text-slate-500 font-semibold text-base group-hover:text-white">
               Catalog
-            </div>
-            <ChevronDownIcon />
-          </Link>
-          <Link
-            href="/wishlist"
-            className="w-32 h-16 flex items-center justify-center hover:bg-amber-500 cursor-pointer group"
-          >
-            <div className="text-slate-500 font-semibold text-base group-hover:text-white">
-              Wishlist
             </div>
           </Link>
           <div className="w-32 h-16 flex items-center justify-center hover:bg-amber-500 cursor-pointer group">
@@ -43,18 +33,12 @@ const Navigation = () => {
           </div>
           <div className="w-32 h-16 flex items-center justify-center hover:bg-amber-500 cursor-pointer group">
             <div className="text-slate-500 font-semibold text-base group-hover:text-white">
-              Pages
-            </div>
-            <ChevronDownIcon />
-          </div>
-          <div className="w-32 h-16 flex items-center justify-center hover:bg-amber-500 cursor-pointer group">
-            <div className="text-slate-500 font-semibold text-base group-hover:text-white">
               About us
             </div>
           </div>
         </div>
         <div className="font-semibold items-center flex mr-16 text-sky-800">
-          <a href="">30 Days Free Return</a>
+          <p>24/7 Support</p>
         </div>
       </div>
     </div>
